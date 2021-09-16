e1d15205f5ef71bf57d94419e00d97ac7d7d169d
rahulguptaamca <53555505+rahulguptaamca@users.noreply.github.com>
2021-09-17 05:29:33 +0530

    remote commit at 17-Sep-52901AM

--- src/Test.java
+++ src/Test.java
@@ -13,10 +13,33 @@ public class Test {
 	}
 	
 	public void remoteMethod1(){
-	String remotevar1="Hello Rahul";
-}
+	String remotevar1="remoteMethod1;
+	System.out.println("remoteMethod1 Change");
+		System.out.println("remoteMethod1 Change1");
+		System.out.println("remoteMethod1 Change2");
+		System.out.println("remoteMethod1 Change3");
+	}
 	public void localMethod1() {
 		String localVar1="Hello localVar1";
 
+	}
+	public void remoteMethod2(){
+	String remoteMethod2="remoteMethod2";
+	}
+	
+	public void remoteMethod3(){
+	String remoteMethod3="remoteMethod3";
+	}
+	
+	public void remoteMethod4(){
+	String remoteMethod4="remoteMethod4";
+	}
+	
+	public void remoteMethod5(){
+	String remoteMethod5="remoteMethod5";
+	}
+	
+	public void remoteMethod6(){
+	String remoteMethod6="remoteMethod6";
 	}
 }
