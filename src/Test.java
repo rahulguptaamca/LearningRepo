90a8c0a7e4f10c860651f754e064b24ab23945ff
Rahul Gupta <rahugupta@tsys.com>
2021-10-14 06:03:06 +0530

    Commit changes from local on14Oct2021 at 6:03AM

--- src/Test.java
+++ src/Test.java
@@ -2,8 +2,8 @@
 public class Test {
 
 	public static void main(String[] args) {
-        System.out.println("New changes directly on remote");
-		System.out.println("New changes1 directly on remote");
+		System.out.println("New changes directly on remote");
+		System.out.println("New changes directly on local"); //local changes done 14oct2021 at 6:00AM
 	}
 
 }
---
+++ src/Test1.java
@@ -0,0 +1,8 @@
+
+public class Test1 {
+ 
+	//added new class Test1 on 14Oct2021 6:01AM
+	public static void main(String[] args) {
+		
+	}
+}
