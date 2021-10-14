010a377ae75a35346accc2c7123cbd9794351f68
rahulguptaamca <53555505+rahulguptaamca@users.noreply.github.com>
2021-10-14 05:59:33 +0530

    remote changes directly.

--- src/Test.java
+++ src/Test.java
@@ -2,8 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-        System.out.println("New changes directly on remote");
-		System.out.println("New changes1 directly on remote");
+        System.out.println("New changes directly on remote");//changes done on 14oct2021 5:54AM
 	}
 
 }
