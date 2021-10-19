4cd552b7e40ef5500f04fa80d3a17c31a4f670a7
Rahul Gupta <rahugupta@tsys.com>
2021-10-19 15:37:13 +0530

    commit changes done in test.java in phase1 branch, change num3

--- src/Test.java
+++ src/Test.java
@@ -1,29 +1,33 @@
 
 public class Test {
 
-
 	public static void main(String[] args) {
-		//Hello Rahul Gupta0
-		//Hello Rahul gupta changes on 16sep730PM
-		//Hello Rahul Gupta
-		//Hello Rahul Changes on 16sep731PM
-		//Hello Rahul Gupta2
-		//Hello Rahul Changes on 16sep73102PM
-		//Changes done on 16sep73202PM
-		//HEllo how are you.
+		// Hello Rahul Gupta0
+		// Hello Rahul gupta changes on 16sep730PM
+		// Hello Rahul Gupta
+		// Hello Rahul Changes on 16sep731PM
+		// Hello Rahul Gupta2
+		// Hello Rahul Changes on 16sep73102PM
+		// Changes done on 16sep73202PM
+		// HEllo how are you.
 	}
-	
+
 	public void afterStashBranch() {
-		
+
 	}
-	
-	//master_phase1 branch change
+
+	// master_phase1 branch change
 	public void afterStashBranch1() {
-		
+
 	}
-	
-	//master_phase1 branch change number2.
-		public void afterStashBranch2() {
-			
-		}
-}	
+
+	// master_phase1 branch change number2.
+	public void afterStashBranch2() {
+
+	}
+
+	// master_phase1 branch change number3.
+	public void afterStashBranch3() {
+
+	}
+}
